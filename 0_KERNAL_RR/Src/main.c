bf3d7e34116d1c39d0586e651304d20ca0cef815
MATRIX-DRAGON <ronypaulchakiyath.0@gmail.com>
2026-02-02 02:03:15 +0530

    modifying the aplication code to demonstrate the prper way to define and use a spinclock handle

--- 0_KERNAL_RR/Src/main.c
+++ 0_KERNAL_RR/Src/main.c
@@ -4,16 +4,16 @@
 #include"user_os_kernal.h"
 
 volatile int task_profile_a,task_profile_b,task_profile_c;
-uint32_t *spinlock_1;
-uint32_t *spinlock_2;
-uint32_t *spinlock_3;
+uint32_t spinlock_1;
+uint32_t spinlock_2;
+uint32_t spinlock_3;
 
 volatile int i=0;
 void user_thread_a(void)
 {
 	while(1)
 		{
-			os_spinlock_wait(spinlock_1);
+			os_spinlock_wait(&spinlock_1);
 			task_profile_a++;
 			led_on();
 			i=0;
@@ -23,7 +23,7 @@ void user_thread_a(void)
 			}
 
 			printf("led ON thread a running \n\r");
-			os_spinlock_set(spinlock_2);
+			os_spinlock_set(&spinlock_2);
 			//os_thread_yeald();
 
 		}
@@ -33,7 +33,7 @@ void user_thread_b(void)
 {
 	while(1)
 		{
-		    os_spinlock_wait(spinlock_2);
+		    os_spinlock_wait(&spinlock_2);
 			task_profile_b++;
 			led_off();
 			i=0;
@@ -43,7 +43,7 @@ void user_thread_b(void)
 			}
 
 			printf("led OFF task b running  \n\r");
-			os_spinlock_set(spinlock_3);
+			os_spinlock_set(&spinlock_3);
 			//os_thread_yeald();
 		}
 
@@ -52,7 +52,7 @@ void user_thread_c(void)
 {
 	while(1)
 		{
-			os_spinlock_wait(spinlock_3);
+			os_spinlock_wait(&spinlock_3);
 			task_profile_c++;
 			led_on();
 			i=0;
@@ -62,7 +62,7 @@ void user_thread_c(void)
 			}
 
 			printf("led ON task c running \n\r");
-			os_spinlock_set(spinlock_1);
+			os_spinlock_set(&spinlock_1);
 			//os_thread_yeald();
 
 		}
@@ -74,12 +74,10 @@ int main()
 	led_init();
 	uart_tx_init();
 
-	spinlock_1 = calloc(1, sizeof(spinlock_1));
-	spinlock_2 = calloc(1, sizeof(spinlock_2));
-	spinlock_3 = calloc(1, sizeof(spinlock_3));
-	os_spinlock_init(spinlock_1,1);
-	os_spinlock_init(spinlock_2,0);
-	os_spinlock_init(spinlock_3,0);
+	os_spinlock_init(&spinlock_1,1);
+	os_spinlock_init(&spinlock_2,0);
+	os_spinlock_init(&spinlock_3,0);
+
 	/*initialise the kernal*/
 	os_kernel_init();
 	/*add three threads*/
